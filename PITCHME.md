9bd77f962f1ec32751b8cd1dac1ce3c081873368
Nicolas Oga <2112.oga@gmail.com>
2019-10-07 10:16:00 -0300

    initial commit

---
+++ PITCHME.md
@@ -0,0 +1,10 @@
+# Python: diccionarios
+
+El diccionario es una estructura de datos, que relaciona claves con valores (key/values).
+
+También conocidos como:
+
+- associative/relational arrays (arreglos relacionales)
+- maps (mapas)
+- hashes
+- table
---
+++ fibbonacci.py
@@ -0,0 +1,21 @@
+# Una funcion que calcula el numéro n en la sequencia de Fibbonacci
+def fibb(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibb(n-1) + fibb(n-2)
+
+# La misma función, pero optimizada mediante memorizar el resultado usando un diccionario
+cache = {}
+def memoized_fibb(n):
+    if not n in cache:
+        if n == 0 or n == 1:
+            cache[n] = n
+        else:
+            cache[n] = memoized_fibb(n-1) + memoized_fibb(n-2)
+
+    return cache[n]
+
+# Imprimo los primeros 100 números de la serie de Fibbonacci
+for n in range(0, 100):
+    print(n, fibb(n))
