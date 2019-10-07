8c83d70d39a7c8e84db11451382acef4d8fb56c4
Nicolas Oga <2112.oga@gmail.com>
2019-10-07 20:53:58 -0300

    Changes

--- fibbonacci2.py
+++ fibbonacci2.py
@@ -1,9 +1,9 @@
 import sys
-import fibbonacci
+from fibbonacci import memoized_fibb
 
 # Otra vez, convierto la string a entero
 max_number = int(sys.argv[1])
 
 # Imprimo los primeros 100 números de la serie de Fibbonacci
 for n in range(0, max_number):
-    print(n, fibbonacci.memoized_fibb(n))
+    print(n, memoized_fibb(n))
